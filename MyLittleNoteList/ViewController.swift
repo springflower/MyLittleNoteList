6c283adc74b625d39f67f578ed1527f29682ad81
Jerry.Lin <springflower9999@gmail.com>
2017-10-25 17:32:02 +0800

    Initial Commit

---
+++ MyLittleNoteList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyLittleNoteList
+//
+//  Created by 林子涵 on 2017/10/25.
+//  Copyright © 2017年 林子涵. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
