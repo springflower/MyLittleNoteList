a03a188eaf1f9942b4d2826ef2af9d3d8fc74112
Jerry.Lin <springflower9999@gmail.com>
2017-10-30 10:39:14 +0800

    0.0.2

--- MyLittleNoteList/ViewController.swift
+++ MyLittleNoteList/ViewController.swift
@@ -8,11 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // 上方加上分隔線
+        let frame = CGRect(x: 0, y: 0, width: self.tableView.frame.size.width, height: 1 / UIScreen.main.scale)
+        let line: UIView = UIView(frame: frame)
+        self.tableView.tableHeaderView = line
+        line.backgroundColor = self.tableView.separatorColor
+        // end of 上方加入分隔線
+        
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +33,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+//MARK - delegate
+    func numberOfSections(in tableView: UITableView) ->Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath)
+        cell.textLabel?.text = "測試標題 \(indexPath.row)"
+        
+        return cell
+    }
 }
 
